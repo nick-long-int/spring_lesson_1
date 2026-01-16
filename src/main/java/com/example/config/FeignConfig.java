734e6a50412a672e2dac7648f1a625f571cb5e8e
nick-long-int <gnidenkonn@yandex.ru>
2026-01-17 01:07:36 +0300

    конечная настройка feign и main класса

--- src/main/java/com/example/config/FeignConfig.java
+++ src/main/java/com/example/config/FeignConfig.java
@@ -2,8 +2,10 @@ package com.example.config;
 
 import com.example.client.UserClient;
 import feign.Feign;
+import feign.Logger;
 import feign.jackson.JacksonDecoder;
 import feign.jackson.JacksonEncoder;
+import feign.slf4j.Slf4jLogger;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -15,6 +17,8 @@ public class FeignConfig {
         return Feign.builder()
             .encoder(new JacksonEncoder())
             .decoder(new JacksonDecoder())
+            .logger(new Slf4jLogger())
+            .logLevel(feign.Logger.Level.FULL)
             .target(UserClient.class, "https://randomuser.me");
     }
 
--- src/main/java/com/example/springhw/SpringHwApplication.java
+++ src/main/java/com/example/springhw/SpringHwApplication.java
@@ -1,20 +1,20 @@
 package com.example.springhw;
 
-import com.example.service.ContactService;
-import com.example.service.ContactServiceImpl;
+import com.example.service.UserService;
+import com.example.service.UserServiceImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.example.service"})
+@ComponentScan(basePackages = {"com.example.service", "com.example.config"})
 public class SpringHwApplication {
 
     public static void main(String[] args) {
         ApplicationContext context = SpringApplication.run(SpringHwApplication.class, args);
-        ContactService service = context.getBean(ContactServiceImpl.class);
-        System.out.println(service.findAll());
+        UserService userService = context.getBean(UserServiceImpl.class);
+        System.out.println(userService.findAll());
     }
 
 }
