b9ef7aa02b514ffd55ea7e9325d49fe8ce66dbc8
nick-long-int <gnidenkonn@yandex.ru>
2026-01-17 01:06:38 +0300

    Создание сервиса и его реализации

---
+++ src/main/java/com/example/service/UserServiceImpl.java
@@ -0,0 +1,23 @@
+package com.example.service;
+
+import com.example.client.UserClient;
+import com.example.dto.UserDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserService {
+
+    private final UserClient userClient;
+
+    @Override
+    public Collection<UserDto> findAll() {
+        return userClient.findAll().getResults();
+    }
+}
